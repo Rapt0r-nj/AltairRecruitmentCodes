a75414979c6d66472d907d79130e5419ea9892fe
Kabir <mahiulkabir.cs@gmail.com>
2024-07-04 04:58:08 +0600

    AI is easyyyyyyyy

---
+++ week2/PythonTasks/220041109_Kabir_w2_py3/find_swan.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLOv10
+import cv2
+import supervision as sv
+
+model = YOLOv10('/content/drive/MyDrive/weights.pt')
+
+def detect_swan(image_path, output_path):
+    image = cv2.imread(image_path)
+    if image is None:
+        print("Error: Unable to load image")
+        return
+
+    results = model(image)[0]
+
+    detections = sv.Detections.from_ultralytics(results)
+
+    bounding_box_annotator = sv.BoundingBoxAnnotator()
+    label_annotator = sv.LabelAnnotator()
+
+    annotated_image = bounding_box_annotator.annotate(scene=image, detections=detections)
+    annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
+
+    cv2.imwrite(output_path, annotated_image)
+
+image_path = './sample.jpg'
+output_path = './output.jpg'
+detect_swan(image_path, output_path)
